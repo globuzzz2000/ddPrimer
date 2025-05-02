177f0b6b52fee8c0fab4e4993303fdb036a1d02d
globuzzz2000 <globus1333@gmail.com>
2025-05-02 17:55:34 +0200

    file removal

--- ddprimer/tests/data/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Test data for ddPrimer tests.
-
-This module provides access to test data used in the ddPrimer test suite.
-It includes sample files for FASTA, VCF, GFF, and other formats needed for testing.
-"""
